e9b84d50d80cfc441f1a217bf602ed9df5d3ba23
Jacob Qumsiyeh <qumsiyeh37@gmail.com>
2025-11-18 20:37:40 +0200

    Finishing 3 more challenges

--- Js - Fundamentals PT.1/Introduction to JS/challenges.js
+++ Js - Fundamentals PT.1/Introduction to JS/challenges.js
@@ -35,3 +35,47 @@ let movieName = 'Southpaw';
 let rating = 9.5;
 
 console.log(`The movie ${movieName} is rated ${rating}/10 by Jacob.`);
+
+// ===============================================
+// Challenge 4 — Age Calculator With Comparison (Operators + typeof)
+// ===============================================
+
+const currentYear = 2025;
+
+const ageJacob = currentYear - 1996;
+const ageFriend = currentYear - 2002;
+
+console.log("Jacob age:", ageJacob);
+console.log("Friend age:", ageFriend);
+
+// Correct comparison
+console.log("Jacob is older:", ageJacob > ageFriend);
+
+// Types
+console.log(typeof ageFriend);
+console.log(typeof ageJacob);
+
+// ===============================================
+// CHALLENGE 5 — Build a Sentence Using Template Literals
+// ===============================================
+
+let firstNameJ = "Jacob";
+let lastNameJ = "Qumsiyeh";
+let myJob = "Full Stack Developer";
+const currentYearJ = 2025;
+const jacobAge = currentYearJ - 1996;
+
+console.log(`My name is ${firstNameJ} ${lastNameJ}, I am ${jacobAge} years old, and I work as a ${myJob}.`);
+
+
+// ===============================================
+// CHALLENGE 6 — Assignment Operator Workout
+// ===============================================
+
+let x = 10;
+x += 5;   // 15
+x *= 3;   // 45
+x -= 2;   // 43
+x /= 4;   // 10.75
+x **= 2;  // 115.5625
+console.log(x);
